3821c76dac051080e185a48f9beb5157fc7e64a1
muzing <muzi2001@foxmail.com>
2022-12-10 22:09:57 +0800

    Add version info to status bar
    在主窗口状态栏上添加版本信息显示功能；

--- src/py2exe_gui/Constants/app_constants.py
+++ src/py2exe_gui/Constants/app_constants.py
@@ -1,11 +1,13 @@
 # Licensed under the GPLv3 License: https://www.gnu.org/licenses/gpl-3.0.html
 # For details: https://github.com/muziing/Py2exe-GUI/blob/main/README.md#license
 
-"""
-应用程序级的常量
-"""
 
-NAME = "Py2exe-GUI"
-VERSION = "0.1.8"
-AUTHORS = ["muzing <muzi2001@foxmail.com>"]
-LICENSE = "GPL-3.0-or-later"
+class AppConstant:
+    """
+    应用程序级的常量
+    """
+
+    NAME = "Py2exe-GUI"
+    VERSION = "0.1.8"
+    AUTHORS = ["muzing <muzi2001@foxmail.com>"]
+    LICENSE = "GPL-3.0-or-later"
