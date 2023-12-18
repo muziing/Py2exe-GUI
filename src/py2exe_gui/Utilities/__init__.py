7ae1210df93a6cc4b467e75e61a23f80662b093e
muzing <muzi2001@foxmail.com>
2023-12-18 08:10:15 +0800

    Add `Utilities`
    添加 `open_dir_in_explorer()`、`get_sys_python()`、`QtFileOpen` 等实用函数与工具类；

---
+++ src/py2exe_gui/Utilities/__init__.py
@@ -0,0 +1,5 @@
+# Licensed under the GPLv3 License: https://www.gnu.org/licenses/gpl-3.0.html
+# For details: https://github.com/muziing/Py2exe-GUI/blob/main/README.md#license
+
+from .open_qfile import QtFileOpen
+from .platform_specifc_funcs import get_sys_python, open_dir_in_explorer
