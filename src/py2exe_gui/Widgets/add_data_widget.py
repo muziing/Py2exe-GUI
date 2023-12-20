6cee2a7c8532766dad3ed63204c5b7667d63581c
muzing <muzi2001@foxmail.com>
2023-12-20 17:22:56 +0800

    Update `AddDataWindow`
    更新添加数据窗口控件：
    添加图标；
    修复无法处理相对路径时会导致崩溃的问题；
    增加自动删除空白行功能；
    修改槽函数命名方式，与其他部分统一；
    其他微小优化；

--- src/py2exe_gui/Widgets/add_data_widget.py
+++ src/py2exe_gui/Widgets/add_data_widget.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Optional
 
 from PySide6.QtCore import QItemSelectionModel, Qt, Signal, Slot
+from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtWidgets import (
     QApplication,
     QFileDialog,
@@ -55,16 +56,16 @@ class AddDataWindow(QWidget):
 
         self._setup_ui()
         self._setup_layout()
+        self._connect_slots()
 
-        self.connect_slots()
-
-    def _setup_ui(self):
+    def _setup_ui(self) -> None:
         """
         处理 UI 内容 \n
         """
 
         self.setWindowTitle("添加文件")
         self.setMinimumWidth(550)
+        self.setWindowIcon(QIcon(QPixmap(":/Icons/Py2exe-GUI_icon_72px")))
 
         self.data_browse_dlg.setFileMode(QFileDialog.FileMode.ExistingFile)
         self.data_dir_browse_dlg.setFileMode(QFileDialog.FileMode.Directory)
@@ -87,7 +88,7 @@ class AddDataWindow(QWidget):
         self.ok_btn.setText("确定")
         self.cancel_btn.setText("取消")
 
-    def _setup_layout(self):
+    def _setup_layout(self) -> None:
         """
         构建与设置布局管理器 \n
         """
@@ -114,65 +115,33 @@ class AddDataWindow(QWidget):
 
         self.setLayout(main_layout)
 
-    def set_work_dir(self, work_dir_path: Path) -> None:
-        self._work_dir = work_dir_path
-        self.data_browse_dlg.setDirectory(str(work_dir_path))
-        self.data_dir_browse_dlg.setDirectory(str(work_dir_path))
-
-    def _submit(self) -> list[data_item]:
-        """
-        将当前界面上的所有配置项转换为 data_item 列表，准备提交给主界面和打包流使用 \n
-        """
-
-        all_data_item_list = []
-        for row in range(self.item_table.rowCount()):
-            path = Path(self.item_table.item(row, 0).text())
-            dest = self.item_table.item(row, 1).text()
-            all_data_item_list.append((path, dest))
-
-        # print(all_data_item_list)  # 调试用
-        return all_data_item_list
-
-    def load_data_item_list(self, all_data_item_list: list[data_item]):
-        """
-        从 data_item 列表加载待添加的数据文件至界面控件
-        """
-
-        self.item_table.clear()
-        self.item_table.setRowCount(len(all_data_item_list))
-        for row, data_item in enumerate(all_data_item_list):
-            source_item = QTableWidgetItem(str(data_item[0].resolve()))
-            source_item.setFlags(
-                Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable
-            )
-            self.item_table.setItem(row, 0, source_item)
-            self.item_table.setItem(row, 1, QTableWidgetItem(data_item[1]))
-
-    def connect_slots(self):
+    def _connect_slots(self) -> None:
         """
         构建各槽函数、连接信号 \n
         """
 
         @Slot()
-        def new_btn_handle():
+        def handle_new_btn():
             # “新建”按钮槽函数
             row_count = self.item_table.rowCount()
-            item = QTableWidgetItem("")
-            item.setFlags(Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable)
+            source_item = QTableWidgetItem("")
+            source_item.setFlags(
+                Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable
+            )
             self.item_table.setRowCount(row_count + 1)
-            self.item_table.setItem(row_count, 0, item)
+            self.item_table.setItem(row_count, 0, source_item)
             self.item_table.setItem(row_count, 1, QTableWidgetItem("."))
             self.item_table.setCurrentCell(
                 row_count, 0, QItemSelectionModel.SelectionFlag.Select
             )  # 自动选中新建行的第一列
 
         @Slot()
-        def delete_btn_handle():
+        def handle_delete_btn():
             # “删除”按钮槽函数
             self.item_table.removeRow(self.item_table.currentRow())
 
         @Slot()
-        def browse_btn_handle():
+        def handel_browse_btn():
             # “浏览文件”按钮槽函数
             if (
                 self.item_table.currentRow() == -1
@@ -184,7 +153,7 @@ class AddDataWindow(QWidget):
             self.data_browse_dlg.open()
 
         @Slot()
-        def browse_dir_btn_handle():
+        def handle_browse_dir_btn():
             # “浏览文件夹”按钮槽函数
             if (
                 self.item_table.currentRow() == -1
@@ -196,7 +165,7 @@ class AddDataWindow(QWidget):
             self.data_dir_browse_dlg.open()
 
         @Slot(str)
-        def browse_selected_handle(file_path: str):
+        def handle_browse_selected(file_path: str):
             """
             处理文件对话框获取到用户打开文件/目录的槽函数 \n
             :param file_path: 用户打开的文件/目录路径
@@ -212,30 +181,74 @@ class AddDataWindow(QWidget):
             )
             self.item_table.setItem(current_row, 0, source_item)
 
-            # DEST 列设置为表示顶层目录的"."或与入口脚本的相对路径
-            dest_item = QTableWidgetItem(str(path.relative_to(self._work_dir)))
-            # FIXME 处理相对路径错误
+            # DEST 列设置为与入口脚本的相对路径或表示顶层目录的"."
             if path.is_dir():
+                try:
+                    dest_item = QTableWidgetItem(str(path.relative_to(self._work_dir)))
+                except ValueError:
+                    dest_item = QTableWidgetItem(str(path.name))
                 self.item_table.setItem(current_row, 1, dest_item)
             else:
                 self.item_table.setItem(current_row, 1, QTableWidgetItem("."))
 
-        def ok_btn_handle():
+        @Slot()
+        def handle_ok_btn():
             # “确定”按钮槽函数
-            # TODO 删掉所有空白行
+
+            # 删掉所有空白行
+            row = 0
+            while row < self.item_table.rowCount():
+                if self.item_table.item(row, 0).text() == "":
+                    self.item_table.removeRow(row)
+                else:
+                    row += 1
+
             self.data_selected.emit(self._submit())
             self.close()
 
         # noinspection DuplicatedCode
-        self.new_btn.clicked.connect(new_btn_handle)
-        self.delete_btn.clicked.connect(delete_btn_handle)
-        self.browse_btn.clicked.connect(browse_btn_handle)
-        self.browse_dir_btn.clicked.connect(browse_dir_btn_handle)
-        self.data_browse_dlg.fileSelected.connect(browse_selected_handle)
-        self.data_dir_browse_dlg.fileSelected.connect(browse_selected_handle)
+        self.new_btn.clicked.connect(handle_new_btn)
+        self.delete_btn.clicked.connect(handle_delete_btn)
+        self.browse_btn.clicked.connect(handel_browse_btn)
+        self.browse_dir_btn.clicked.connect(handle_browse_dir_btn)
+        self.data_browse_dlg.fileSelected.connect(handle_browse_selected)
+        self.data_dir_browse_dlg.fileSelected.connect(handle_browse_selected)
 
-        self.ok_btn.clicked.connect(ok_btn_handle)
+        self.ok_btn.clicked.connect(handle_ok_btn)
         self.cancel_btn.clicked.connect(self.close)
+
+    def _submit(self) -> list[data_item]:
+        """
+        将当前界面上的所有配置项转换为 data_item 列表，准备提交给主界面和打包流使用 \n
+        """
+
+        all_data_item_list = []
+        for row in range(self.item_table.rowCount()):
+            path = Path(self.item_table.item(row, 0).text())
+            dest = self.item_table.item(row, 1).text()
+            all_data_item_list.append((path, dest))
+
+        return all_data_item_list
+
+    def set_work_dir(self, work_dir_path: Path) -> None:
+        self._work_dir = work_dir_path
+        self.data_browse_dlg.setDirectory(str(work_dir_path))
+        self.data_dir_browse_dlg.setDirectory(str(work_dir_path))
+
+    def load_data_item_list(self, all_data_item_list: list[data_item]) -> None:
+        """
+        从 data_item 列表加载待添加的数据文件至界面控件
+        :param all_data_item_list: 保存数据条目的列表
+        """
+
+        self.item_table.setRowCount(len(all_data_item_list))
+        for row, data_item in enumerate(all_data_item_list):
+            source_item = QTableWidgetItem(str(data_item[0].resolve()))
+            source_item.setFlags(
+                Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable
+            )
+            self.item_table.setItem(row, 0, source_item)
+            self.item_table.setItem(row, 1, QTableWidgetItem(data_item[1]))
 
 
 if __name__ == "__main__":
