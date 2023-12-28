a7cb5aa7224ef3265dd9f9d8f634d60848061875
muzing <muzi2001@foxmail.com>
2023-12-28 10:57:02 +0800

    Add package-level docstrings
    增加与完善软件包级文档字符串

--- src/py2exe_gui/Constants/__init__.py
+++ src/py2exe_gui/Constants/__init__.py
@@ -1,9 +1,7 @@
 # Licensed under the GPLv3 License: https://www.gnu.org/licenses/gpl-3.0.html
 # For details: https://github.com/muziing/Py2exe-GUI/blob/main/README.md#license
 
-"""
-各类常量与全局变量
-"""
+"""各类常量、枚举值与全局变量"""
 
 from .app_constants import *
 from .packaging_constants import *
--- src/py2exe_gui/Widgets/__init__.py
+++ src/py2exe_gui/Widgets/__init__.py
@@ -1,6 +1,9 @@
 # Licensed under the GPLv3 License: https://www.gnu.org/licenses/gpl-3.0.html
 # For details: https://github.com/muziing/Py2exe-GUI/blob/main/README.md#license
 
+"""本 package 中包含所有控件类，集中处理界面（前端）相关功能
+"""
+
 from .add_data_widget import AddDataWindow
 from .arguments_browser import ArgumentsBrowser
 from .center_widget import CenterWidget
