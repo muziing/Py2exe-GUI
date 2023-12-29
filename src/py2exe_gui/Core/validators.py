972c379277e172363ef10bbf26661c97948fe953
muzing <muzi2001@foxmail.com>
2023-12-29 15:58:42 +0800

    Update `validators`
    更新验证器模块：
    添加与完善docstrings；
    完善三个验证方法的实现；

--- src/py2exe_gui/Core/validators.py
+++ src/py2exe_gui/Core/validators.py
@@ -1,96 +1,95 @@
 # Licensed under the GPLv3 License: https://www.gnu.org/licenses/gpl-3.0.html
 # For details: https://github.com/muziing/Py2exe-GUI/blob/main/README.md#license
 
+"""此模块包含数个验证器类，用于验证用户输入是否有效
+
+`FilePathValidator.validate_script()` 用于验证用户给定的路径是否为有效的 Python 脚本；
+`FilePathValidator.validate_icon()` 用于验证用户给定的图标文件是否有效；
+`InterpreterValidator.validate()` 用于验证用户给定的路径是否为有效的 Python 解释器可执行文件；
+"""
+
 import os
 import subprocess
 from pathlib import Path
 from typing import Union
 
+from ..Constants import RUNTIME_INFO, Platform
+
 
 class FilePathValidator:
-    """
-    根据给定的路径验证文件的有效性
-    """
+    """根据给定的路径验证文件的有效性"""
 
     @classmethod
     def validate_script(cls, script_path: Union[str, Path]) -> bool:
-        """
-        验证脚本路径是否有效 \n
+        """验证脚本路径是否有效
+
         :param script_path: 脚本路径
+        :return:脚本路径是否有效
         """
 
         path = Path(script_path)
-        # TODO 完善验证方法
-        return path.exists() and path.is_file()
+
+        # 文件是否存在
+        if not path.exists() or not path.is_file():
+            return False
+
+        # 是否对文件有读取权限
+        if not os.access(script_path, os.R_OK):
+            return False
+
+        return True
 
     @classmethod
     def validate_icon(cls, icon_path: Union[str, Path]) -> bool:
-        """
-        验证图标路径是否有效 \n
+        """验证图标路径是否有效
+
         :param icon_path: 图标路径
         """
 
         path = Path(icon_path)
-        # TODO 完善验证方法，根据 Windows(.ico)、macOS(.icns) 动态分析
-        return path.exists() and path.is_file()
+
+        if not path.exists() and path.is_file():
+            return False
+
+        # TODO 如果安装了可选依赖 Pillow，则进行更精确的判断和尝试转换图像格式
+        if RUNTIME_INFO.platform == Platform.windows:
+            return path.suffix == ".ico"
+        elif RUNTIME_INFO.platform == Platform.macos:
+            return path.suffix == ".icns"
+
+        return True
 
 
 class InterpreterValidator:
-    """
-    验证给定的可执行文件是否为有效的Python解释器 \n
-    """
-
-    def __init__(self, path: Union[str, os.PathLike[str]]) -> None:
-        """
-        :param path: 可执行文件路径
-        """
-
-        self._itp_path: Path = Path(path)
-        self._itp_validated: bool = False
-        self.validate_itp()
-
-    def validate_itp(self) -> bool:
-        """
-        :return: 解释器是否有效
-        """
-
-        if (
-            self._itp_path.exists()  # 该路径存在
-            and self._itp_path.is_file()  # 为文件
-            and self._itp_path.name.startswith("python")  # 文件名以python起始
-        ):
-            try:
-                # 尝试将该文件作为Python解释器运行
-                subprocess_args = [
-                    str(self._itp_path.resolve()),
-                    "-c",
-                    "import sys",
-                ]
-                result = subprocess.run(
-                    args=subprocess_args,
-                    timeout=300,
-                )
-                if result.returncode == 0:
-                    self._itp_validated = True
-                    return True
-                else:
-                    self._itp_validated = False
-                    return False
-            except OSError:
-                self._itp_validated = False
-                return False
-        else:
-            self._itp_validated = False
-            return False
+    """验证给定的可执行文件是否为有效的Python解释器"""
 
     @classmethod
-    def validate(cls, path: Union[str, os.PathLike[str]]) -> bool:
-        """
-        验证path是否指向有效的Python解释器 \n
+    def validate(cls, path: Union[str, Path, os.PathLike[str]]) -> bool:
+        """验证 `path` 是否指向有效的Python解释器
+
         :return: 是否有效
         """
 
-        return InterpreterValidator(path).validate_itp()
+        itp_path = Path(path).absolute()
+
+        if not (
+            itp_path.exists()  # 该路径存在
+            and itp_path.is_file()  # 为文件
+            and os.access(itp_path, os.X_OK)  # 可执行权限
+        ):
+            return False
+
+        try:
+            # 尝试将该文件作为Python解释器运行
+            subprocess_args = [str(itp_path), "-c", "import sys"]
+            subprocess.run(args=subprocess_args, timeout=0.3, check=True)
+        except subprocess.SubprocessError:
+            return False
+        except OSError as e:
+            print(f"无法启动Python解释器有效性验证子进程：{e}")
+            return False
+        else:
+            return True
 
 
 if __name__ == "__main__":
